50a2b23327b98e68e23b57854833993cb730996d
UrvikP <urvik95@gmail.com>
2023-03-30 14:42:59 -0400

    Created new component Alert

--- GameLibraryApp/src/App.tsx
+++ GameLibraryApp/src/App.tsx
@@ -1,17 +1,22 @@
-import ListGroup from "./components/ListGroup";
-
+//import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 function App() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  //const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  //const handleSelectItem = (item: string) => {
+  //  console.log(item);
+  //};
   return (
+    //<div>
+     // <ListGroup
+     //   items={items}
+     //   heading="Cities"
+     //   onSelectItem={handleSelectItem}
+     // />
+    //</div>
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+      Hello World
+      </Alert>
     </div>
   );
 }
