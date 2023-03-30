8cf7312ab593bef6f37d0412a01ec0765aaf67c0
UrvikP <urvik95@gmail.com>
2023-03-30 15:44:06 -0400

    Gave button functionality

--- GameLibraryApp/src/App.tsx
+++ GameLibraryApp/src/App.tsx
@@ -1,11 +1,13 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import { useState } from "react";
 function App() {
   //const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   //const handleSelectItem = (item: string) => {
   //  console.log(item);
   //};
+  const [alertVisibile, setAlertVisibilty] = useState(false);
   return (
     //<div>
     // <ListGroup
@@ -20,8 +22,8 @@ function App() {
     //  </Alert>
     //</div>
     <div>
-      <button onClick={ () => console.log('Clicked')}> My button</button>
-      <button color='secondary' onClick={() => console.log('Clicked')}>My Button</button>
+      {alertVisibile && <Alert onClose={() => setAlertVisibilty(false)}>My Alert</Alert>}
+      <Button color='secondary' onClick={() => setAlertVisibilty(true)}>My Button</Button>
     </div>
   );
 }
